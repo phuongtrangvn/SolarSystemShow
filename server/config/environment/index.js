7e669d741ea4c4054bc35bf0a6f6126519c0f2f6
LT-PC\LT <linhnt28031995@gmail.com>
2017-03-30 02:04:41 +0700

    deploy heroky config

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -46,4 +46,4 @@ var all = {
 // ==============================================
 module.exports = _.merge(
   all,
-  require('./' + process.env.NODE_ENV + '.js') || {});
+  require('./' + (process.env.NODE_ENV || 'production') + '.js') || {});
